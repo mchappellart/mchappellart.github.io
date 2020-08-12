1831f15c7af3882dc244ed5715546c1597233b57
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-08-12 21:28:17 +1000

    removed portfolio folder and updated responsiveness

--- preview2/js/footer.js
+++ preview2/js/footer.js
@@ -3,14 +3,14 @@ document.getElementById('footer').innerHTML =
 <div class="container-fluid">
   <div class="row align-items-center py-4">
     <div class="col-6">
-      <a href="./index.html"><img src="./img/logo.png" class="d-inline ml-4 d-block d-md-none" width="30">
+      <a href="./index.html"><img src="./img/logo.png" class="d-inline ml-0 ml-md-4 d-block d-md-none" width="30">
         <p class="d-none d-sm-none d-md-block"><img src="./img/logo.png" class="d-inline ml-4" width="30"><i class="far fa-circle ml-4 mr-3 smaller"></i>MADELEINE CHAPPELL</p></a>
     </div>
     <div class="col-6 d-flex justify-content-end">
       <p>
         <a href="https://twitter.com/notguilmug" target="_blank"><i class="fab fa-twitter bigger mx-1"></i></a>
-        <a href="https://www.instagram.com/mchappellart/?hl=en" target="_blank"><i class="fab fa-twitch mx-2"></i></a>
-        <a href="https://www.linkedin.com/in/madeleine-chappell/" target="_blank"><i class="fab fa-linkedin-in bigger ml-1 mr-4"></i></a>
+        <a href="https://www.instagram.com/mchappellart/?hl=en" target="_blank"><i class="fab fa-twitch mx-1"></i></a>
+        <a href="https://www.linkedin.com/in/madeleine-chappell/" target="_blank"><i class="fab fa-linkedin-in bigger mx-1"></i></a>
       </p>
     </div>
   </div>
