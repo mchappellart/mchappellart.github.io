e97977aecf5bf17a612cdece4f430e753f292b08
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-08-31 14:50:43 +1000

    Update contact responsiveness

--- js/footer.js
+++ js/footer.js
@@ -2,11 +2,11 @@ document.getElementById('footer').innerHTML =
 `
 <div class="container-fluid">
   <div class="row align-items-center py-4">
-    <div class="col-6">
+    <div class="col-6 col-sm-8">
       <a href="./index.html"><img src="./img/logo.png" class="d-inline ml-0 ml-md-4 d-block d-md-none" width="30">
         <p class="d-none d-sm-none d-md-block"><img src="./img/logo.png" class="d-inline ml-4" width="30"><i class="far fa-circle ml-4 mr-3 smaller"></i>MADELEINE CHAPPELL</p></a>
     </div>
-    <div class="col-6 d-flex justify-content-end">
+    <div class="col-6 col-sm-4 d-flex justify-content-end">
       <p>
         <a href="https://twitter.com/notguilmug" target="_blank"><i class="fab fa-twitter bigger mx-1"></i></a>
         <a href="https://www.instagram.com/mchappellart/?hl=en" target="_blank"><i class="fab fa-twitch bigger mx-1"></i></a>
